28a61c249c5951f8c07887ed366945fb2d79568e
sander-aaron <31959117+sander-aaron@users.noreply.github.com>
2019-03-19 15:17:32 -0400

    Add all the things

--- js/main.js
+++ js/main.js
@@ -1,25 +1,11 @@
-/*
-MIXED UP LINES OF CODE
-============================
-
-if (!isOpen) {
-
-$(this).addClass('info-panel-btn-active');
-
-$('.info-panel').attr('hidden', true);
-
 $('.info-panel-btn').on('click', function () {
-
-$(id).removeAttr('hidden');
-
-var isOpen = $(this).hasClass('info-panel-btn-active');
-
-$('.info-panel-btn').removeClass('info-panel-btn-active');
-
-var id = $(this).attr('href');
-
-}
+  var isOpen = $(this).hasClass('info-panel-btn-active');
+  var id = $(this).attr('href');
+  $('.info-panel').attr('hidden', true);
+  $('.info-panel-btn').removeClass('info-panel-btn-active');
+  if (!isOpen) {
+    $(this).addClass('info-panel-btn-active');
+    $(id).removeAttr('hidden');
+  }
 
 });
-
-*/
